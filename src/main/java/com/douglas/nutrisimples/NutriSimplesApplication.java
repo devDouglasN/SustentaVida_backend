940edc4fcc951ef9312f14d5501a963ad62b2def
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-28 19:06:07 -0300

    Project started

---
+++ src/main/java/com/douglas/nutrisimples/NutriSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.nutrisimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriSimplesApplication.class, args);
+	}
+
+}
