b876bfc0e9869601e83cc40ba50c4b15b1616ae3
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-29 15:52:57 -0300

    JPA annotations for entities

--- src/main/java/com/douglas/nutrisimples/model/Product.java
+++ src/main/java/com/douglas/nutrisimples/model/Product.java
@@ -2,12 +2,19 @@ package com.douglas.nutrisimples.model;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "tb_classification")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -15,6 +22,8 @@ import lombok.Setter;
 @EqualsAndHashCode(of = "id")
 public class Product {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private BigDecimal price;
