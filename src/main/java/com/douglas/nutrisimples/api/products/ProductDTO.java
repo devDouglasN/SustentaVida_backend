92996369aa8e7b6ba292b6b15b2af96439586c67
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-02-21 22:37:48 -0300

    Add input validations to ProductDTO

--- src/main/java/com/douglas/nutrisimples/api/products/ProductDTO.java
+++ src/main/java/com/douglas/nutrisimples/api/products/ProductDTO.java
@@ -4,6 +4,11 @@ import java.math.BigDecimal;
 
 import com.douglas.nutrisimples.domain.Product;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -18,9 +23,20 @@ import lombok.Setter;
 public class ProductDTO {
 
 	private Long id;
+	
+	@NotBlank(message = "Name cannot be blank")
+    @Size(max = 100, message = "Name cannot have more than 100 characters")
 	private String name;
+	
+	@NotNull(message = "Price cannot be null")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than zero")
 	private BigDecimal price;
+	
+	@NotNull(message = "Amount cannot be null")
+    @Min(value = 0, message = "Amount cannot be less than zero")
 	private Integer amount;
+	
+	@Size(max = 200, message = "Description cannot have more than 200 characters")
 	private String description;
 	
 	public ProductDTO(Product obj) {
