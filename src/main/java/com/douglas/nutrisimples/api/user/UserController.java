2042cf1dabaf5759876706a269e9b1682b82dfea
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-04-09 23:11:56 -0300

    Method to authenticate user

--- src/main/java/com/douglas/nutrisimples/api/user/UserController.java
+++ src/main/java/com/douglas/nutrisimples/api/user/UserController.java
@@ -1,11 +1,15 @@
 package com.douglas.nutrisimples.api.user;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +33,13 @@ public class UserController {
 	public ResponseEntity<User> getById(@PathVariable Long id) {
 		User object = userService.findById(id);
 		return ResponseEntity.ok().body(object);
+	}
+
+	@PostMapping("/logar")
+	public ResponseEntity<User> autenticationUser(@RequestBody Optional<User> userLogin){
+
+		return userService.autenticarUsuario(userLogin)
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 }
