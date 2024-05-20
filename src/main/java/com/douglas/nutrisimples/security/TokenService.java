acbb1046e87b42b4cfe6a7195aa8498a4313081f
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-20 19:06:33 -0300

    Refactor token generation

--- src/main/java/com/douglas/nutrisimples/security/TokenService.java
+++ src/main/java/com/douglas/nutrisimples/security/TokenService.java
@@ -10,28 +10,43 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 @Service
 public class TokenService {
 	
 	@Value("${api.security.token.secret}")
-    private String secret;
-
-    public String generateToken(UserSS user) {
-        try {
-            var algorithm = Algorithm.HMAC256(secret);
-            String token = JWT.create()
-                    .withSubject(user.getUsername())
-                    .withIssuer("API ReadVista")
-                    .withExpiresAt(expirationDate())
-                    .sign(algorithm);
-            return token;
-        } catch (JWTCreationException exception){
-            throw new RuntimeException("Error generating jwt token", exception);
-        }
-    }
-
-    private Instant expirationDate() {
-        return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-03:00"));
-    }
+	private String secret;
+	
+	public String generateToken(UserSS user) {
+		try {
+		    var algoritimo = Algorithm.HMAC256(secret);
+		    return JWT.create()
+		        .withIssuer("API Medi.pro")
+		        .withSubject(user.getUsername())
+		        .withExpiresAt(expirationDate())
+		        .sign(algoritimo);
+		} catch (JWTCreationException exception){
+			throw new RuntimeException("erro ao gerar token jwt", exception);
+		}
+	}
+	
+	public String getSubject (String tokenJWT) {
+		try {
+			var algoritimo = Algorithm.HMAC256(secret);
+			return JWT.require(algoritimo)
+					.withIssuer("API Medi.pro")
+					.build()
+					.verify(tokenJWT)
+					.getSubject();
+		} catch (JWTVerificationException exception) {
+			throw new RuntimeException("TokenJWT inválido ou expirado");
+		}
+		
+	}
+	
+	private Instant expirationDate() {
+		return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+	}
 }
+
