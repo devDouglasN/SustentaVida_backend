dc44376cb1bdac4db8d5e29bc47417129af850e3
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-20 19:09:34 -0300

    User API Refactor

--- src/main/java/com/douglas/nutrisimples/api/user/UserController.java
+++ src/main/java/com/douglas/nutrisimples/api/user/UserController.java
@@ -1,7 +1,7 @@
 package com.douglas.nutrisimples.api.user;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +9,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.douglas.nutrisimples.domain.User;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
@@ -34,12 +39,19 @@ public class UserController {
 		User object = userService.findById(id);
 		return ResponseEntity.ok().body(object);
 	}
-
-	@PostMapping("/logar")
-	public ResponseEntity<User> autenticationUser(@RequestBody Optional<User> userLogin){
-
-		return userService.autenticarUsuario(userLogin)
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
-				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserDTO objDTO) {
+		User obj = userService.update(id, objDTO);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> create(@RequestBody UserDTO objDTO) {
+		User newObj = userService.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- src/main/java/com/douglas/nutrisimples/api/user/UserService.java
+++ src/main/java/com/douglas/nutrisimples/api/user/UserService.java
@@ -1,24 +1,43 @@
 package com.douglas.nutrisimples.api.user;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.douglas.nutrisimples.domain.User;
+import com.douglas.nutrisimples.repositories.UserRepository;
+import com.douglas.nutrisimples.security.exceptions.ObjectNotFoundException;
+
+import jakarta.validation.Valid;
 
 @Service
 public class UserService {
 
 	@Autowired
 	public UserRepository userRepository;
-
+	
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-
-	public User findById(Integer id) {
+	
+	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Id object: " + id + " not found"));
 	}
+	
+	public User update(Long id, @Valid UserDTO objDTO) {
+		objDTO.setId(id);
+		User oldObj = findById(id);
+		oldObj = new User(objDTO);
+		return userRepository.save(oldObj);	
+	}
+	
+	public User create(UserDTO objDTO) {
+		objDTO.setId(null);
+		User newObj = new User(objDTO);
+		return userRepository.save(newObj);
+	}
+
 }
