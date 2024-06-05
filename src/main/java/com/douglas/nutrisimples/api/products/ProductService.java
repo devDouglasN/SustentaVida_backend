08aa2d93403f403674d6deb977891ae7b3607cc5
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-06-04 23:56:24 -0300

    Refactor service logic

--- src/main/java/com/douglas/nutrisimples/api/products/ProductService.java
+++ src/main/java/com/douglas/nutrisimples/api/products/ProductService.java
@@ -8,31 +8,59 @@ import org.springframework.stereotype.Service;
 
 import com.douglas.nutrisimples.domain.Product;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class ProductService {
-	
-	@Autowired
-	public ProductRepository productRepository;
+    
+    @Autowired
+    private ProductRepository productRepository;
 
-	public List<Product> findAll() {
-		return productRepository.findAll();
-	}
-	
-	public Product findById(Long id) {
-		Optional<Product> product = productRepository.findById(id);
-		return product.orElseThrow(() -> new RuntimeException("Object not found! ID:" + id));
-	}
-	
-	public Product create(ProductDTO objectDTO) {
-		objectDTO.setId(null);
-		Product newObj = new Product(objectDTO);
-	    return productRepository.save(newObj);
-	}
-	
-	public Product update(Long id, ProductDTO objectDTO) {
-	    Product obj = findById(id);
-	    obj.setName(objectDTO.getName());
-	    obj.setPrice(objectDTO.getPrice());
-	    return productRepository.save(obj);
-	}
+    public List<Product> findAll() {
+        log.info("Listando todos os produtos");
+        return productRepository.findAll();
+    }
+    
+    public Product findById(Long id) {
+        log.info("Buscando produto de ID: " + id);
+        Optional<Product> product = productRepository.findById(id);
+        return product.orElseThrow(() -> new RuntimeException("Objeto não encontrado! ID: " + id));
+    }
+    
+    public Product create(ProductDTO objectDTO) {
+        log.info("Criando novo produto: " + objectDTO.name());
+        Product newObj = new Product();
+        newObj.setName(objectDTO.name());
+        newObj.setPrice(objectDTO.price());
+        newObj.setAmount(objectDTO.amount());
+        newObj.setDescription(objectDTO.description());
+        Product savedProduct = productRepository.save(newObj);
+        log.info("Produto criado com sucesso: " + savedProduct);
+        return savedProduct;
+    }
+    
+    public Product update(Long id, ProductDTO objectDTO) {
+        log.info("Atualizando produto de ID: " + id);
+        Product obj = findById(id);
+        obj.setName(objectDTO.name());
+        obj.setPrice(objectDTO.price());
+        obj.setAmount(objectDTO.amount());
+        obj.setDescription(objectDTO.description());
+        Product updatedProduct = productRepository.save(obj);
+        log.info("Produto atualizado com sucesso: " + updatedProduct);
+        return updatedProduct;
+    }
+
+    public boolean deleteById(Long id) {
+        log.info("Deletando produto de ID: " + id);
+        if (productRepository.findById(id).isPresent()) {
+            productRepository.deleteById(id);
+            log.info("Produto deletado com sucesso: ID " + id);
+            return true;
+        }
+        log.warn("Produto com ID " + id + " não encontrado.");
+        return false;
+    }
+    
 }
