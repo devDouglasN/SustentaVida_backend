ffc81403172deee3afed029c95e9a3de9953c9c3
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-06-13 20:38:35 -0300

    Improvement in the authentication service

--- src/main/java/com/douglas/nutrisimples/infra/AuthenticationService.java
+++ src/main/java/com/douglas/nutrisimples/infra/AuthenticationService.java
@@ -1,20 +1,35 @@
 package com.douglas.nutrisimples.infra;
 
+import com.douglas.nutrisimples.domain.User;
+import com.douglas.nutrisimples.api.user.UserRepository;
+import com.douglas.nutrisimples.userss.UserSS;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import com.douglas.nutrisimples.userss.UserSSRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AuthenticationService implements UserDetailsService {
-	
+
 	@Autowired
-    private UserSSRepository repository;
+	private UserRepository userRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		return repository.findByEmail(email);
+		Optional<User> user = userRepository.findByEmail(email);
+		List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+
+		if(user.isPresent()) {
+			return new UserSS(user.get().getId(), user.get().getEmail(), user.get().getPassword(), authorities);
+		}
+		throw new UsernameNotFoundException("Usuário não encontrado com e-mail: " + email);
 	}
 
 }
