449ed7210f7b75881f0cde8b8e113cd601a0cccc
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-06-13 19:09:48 -0300

    Unnecessary classes deleted

--- src/main/java/com/douglas/nutrisimples/api/user/UserRepository.java
+++ src/main/java/com/douglas/nutrisimples/api/user/UserRepository.java
@@ -3,16 +3,10 @@ package com.douglas.nutrisimples.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import com.douglas.nutrisimples.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	UserDetails findByLogin(String login);
-
-	UserDetails findByEmail(String email);
-
-	Optional<User> findByUsuario(String name);
-	
+	Optional<User> findByEmail(String email);
 }
